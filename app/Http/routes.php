cfe2605eaa504fc4dab93e5f08a72124e6462b6e
Matthew Bucci <buccim2@gene.com>
2015-08-06 13:16:01 -0700

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,22 @@ Route::get('me', 'UserController@me');
 
 
 Route::get('track', 'TrackController@index');
+Route::get('track/trending', 'TrackController@trending');
+Route::get('track/popular', 'TrackController@popular');
+Route::get('track/newest', 'TrackController@newest');
+Route::get('track/{id}', 'TrackController@byId');
+Route::post('track/{id}/favorite', 'TrackController@favorite');
+Route::post('track/{id}/flag', 'TrackController@flag');
+
+Route::get('artist', 'ArtistController@index');
+Route::get('artist/trending', 'ArtistController@trending');
+Route::get('artist/popular', 'ArtistController@popular');
+Route::get('artist/newest', 'ArtistController@newest');
+Route::get('artist/{id}', 'TrackController@byId');
+
+Route::get('genres', 'GenreController@index');
+Route::get('genres/{name}/artists', 'ArtistController@byGenre');
+Route::get('genres/{name}/communities', 'CommunityController@byGenre');
 
 Route::get('communities', 'CommunityController@index');
 Route::get('communities/trending', 'CommunityController@trending');
@@ -23,6 +39,8 @@ Route::get('communities/popular', 'CommunityController@popular');
 Route::get('communities/newest', 'CommunityController@newest');
 Route::get('community/{name}', 'CommunityController@byName');
 Route::get('community/{name}/tracks', 'TrackController@byCommunity');
+Route::post('communities/{name}', 'CommunityController@create');
+Route::put('communities/{name}', 'CommunityController@update');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
